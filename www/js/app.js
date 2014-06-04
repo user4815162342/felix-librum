5082cc66106079fc52b83e0c62262d55f425cb0a
user4815162342 <neilmsheldon@townsedgetechnology.com>
2014-06-04 15:00:27 -0500

    -- Developed the basics of the interface. System is now able to load the JSON files and filter them by title, author and subject.

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,25 @@
 'use strict';
 
+/* 
+ * TODO: In the index files, the main name should be 'name', not 'author', 'title', etc.
+ * TODO: The search queries should automatically run when someone presses enter.
+ * TODO: The 'author1..3' should probably be an array of names and roles (illustrator, editor, etc.)
+ * TODO: May Need to break up lots of results data into smaller chunks for quicker loading. 
+ * Probably have to paginate them somehow.
+ * TODO: For title search, need to be able to know whether the string is
+ * from the title, subtitle or series name.
+ * TODO: The item-details page should just be able to display a whole bunch of results,
+ * and then the user can just click on the 'title' or 'subject' found and get the result.
+ * TODO: Need to clean up the user interface quite a bit.
+ * TODO: Need Loading animations 
+ * TODO: Need to filter and search.
+ * TODO: Need to figure out what those unknown fields are.
+ * TODO: Goal: A purely JavaScript driven interface, so it doesn't
+ * have to depend on the architecture of the back end. User should be
+ * able to search by title, and author and subject.
+ * 
+ * */
+
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
@@ -10,7 +30,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/items/:queryType?/:searchKey?', {templateUrl: 'partials/items.html', controller: 'itemsController'})
+  $routeProvider.when('/item/:itemKey', {templateUrl: 'partials/item-detail.html', controller: 'itemDetailController'});
+  $routeProvider.otherwise({redirectTo: '/items/'});
 }]);
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,9 +3,59 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+    .controller('itemsController', ['$scope', '$routeParams', '$filter', '$http', function($scope,$routeParams,$filter,$http) {
+      // NOTE: We are doing this here instead of in an 'instant filter'
+      // with databinding, because it is much more responsive considering
+      // the amount of data we start out with. Although, there may
+      // be better ways to do this.
+      
+        var queryType = $routeParams.queryType && $routeParams.queryType.toLowerCase();
+        var searchKey = $routeParams.searchKey && $routeParams.searchKey.toLowerCase();
+        $scope.query = {
+            type: queryType,
+            key: searchKey
+        }
+        
+        $http.get('data/items.json').success(function(data) {
+            $scope.all = data;
+            switch ($scope.query.type) {
+                case "title":
+                    $scope.items = $filter('filter')(data,function(value) {
+                        return (value.title.toLowerCase().indexOf(searchKey) > -1) ||
+                                (value.subtitle.toLowerCase().indexOf(searchKey) > -1) ||
+                                (value.series.toLowerCase().indexOf(searchKey) > -1)
+                    });
+                    break;
+                case "author":
+                    $scope.items = $filter('filter')(data,function(value) {
+                        return (value.author1.toLowerCase().indexOf(searchKey) > -1) ||
+                                (value.author2.toLowerCase().indexOf(searchKey) > -1) ||
+                                (value.author3.toLowerCase().indexOf(searchKey) > -1)
+                    });
+                    break;
+                case "subject":
+                    $scope.items = $filter('filter')(data,function(value) {
+                        for (var i = 0; i < value.subjects.length; i++) {
+                            if (value.subjects[i].toLowerCase().indexOf(searchKey) > -1) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    });
+                    break;
+                case "everything":
+                    $scope.items = $filter('filter')(data,{ $: searchKey });
+                    break;
+                case "all":
+                    $scope.items = data;
+                    break;
+            }
+        });
+    }])
+  .controller('itemDetailController', ['$scope', '$routeParams', '$http', function($scope,$routeParams,$http) {
+      $scope.itemKey = $routeParams.itemKey;
+      $http.get('data/' + $scope.itemKey + '.json').success(function(data) {
+          $scope.item = data;
+      });
 
   }]);
