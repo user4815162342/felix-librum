aea573e8e61952938f6e389cd1541dc24bd308ad
user4815162342 <neilmsheldon@townsedgetechnology.com>
2014-06-04 13:23:22 -0500

    -- Added app name directive.

--- www/js/directives.js
+++ www/js/directives.js
@@ -8,4 +8,9 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
+  }]).
+  directive('appName', ['name', function(name) {
+      return function(scope, elm, attrs) {
+          elm.text(name);
+      };
   }]);
