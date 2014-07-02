ebbee2ebb6520056e21902269a15631aff97c8f9
user4815162342 <neilmsheldon@townsedgetechnology.com>
2014-07-02 16:39:34 -0500

    -- Got e2e testing working with mock data.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,38 +4,60 @@
 
 describe('my app', function() {
 
+  // need some mock data in the application. This appears to
+  // be the easiest way to do this. It required me to move the
+  // dataAccess into a separate service, but that's probably a good
+  // thing anyway.
+  var mock = require("../mock/items");
+  var mockScript = "angular.module('myApp.services').factory('dataAccess',['$timeout',function($timeout) { \
+       return {\
+            getItems: function(cb) { \
+                $timeout(function() {\
+                    cb(null," + JSON.stringify(mock.items) + "); \
+                },1000);\
+            },\
+            \
+            getItem: function(id,cb) {\
+                $timeout(function() {\
+                    cb(null," + JSON.stringify(mock.item20) + "); \
+                },1000);\
+            }\
+       } \
+  }])";
+
+  browser.addMockModule('myApp.services',mockScript);
   browser.get('index.html');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+  it('should automatically redirect to /list when location hash/fragment is empty', function() {
+    expect(browser.getLocationAbsUrl()).toMatch("/list");
+  });
+
+  // TODO: Should put some user interface scenarios in here.
+  describe('list', function() {
+
+    beforeEach(function() {
+      browser.get('index.html#/list');
+    });
+
+
+    it('should render itemsController when user navigates to /list', function() {
+      expect(element.all(by.css('[ng-view] div')).first().getText()).
+        toMatch(/The library has .* items in its catalog/);
+    });
+
   });
 
 
-  describe('view1', function() {
+  describe('details', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('index.html#/detail/1');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+    it('should render itemsDetail when user navigates to /detail', function() {
+      expect(element.all(by.css('[ng-view] div')).first().getText()).
+        toMatch(/Back to Results/);
     });
 
   });
